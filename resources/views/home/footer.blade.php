1fe63e4e0e01ba7216cd593f94b6ec77b2037e72
blueswayne133 <blueswayne133@gmail.com>
2025-07-09 14:06:11 -0700

    update

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -32,11 +32,18 @@
     </div>
 
 
-    <!--Add the following script at the bottom of the web page (before </body></html>)-->
-    <script type="text/javascript">
-        function add_chatinline(){var hccid=25555038;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}
-add_chatinline();
+    <!-- Start of LiveChat (www.livechat.com) code -->
+    <script>
+        window.__lc = window.__lc || {};
+    window.__lc.license = 19229030;
+    window.__lc.integration_name = "manual_channels";
+    window.__lc.product_name = "livechat";
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
     </script>
+    <noscript><a href="https://www.livechat.com/chat-with/19229030/" rel="nofollow">Chat with us</a>, powered by <a
+            href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a></noscript>
+    <!-- End of LiveChat code -->
+
     {{--
     <!-- Begin of Chaport Live Chat code -->
     <script type="text/javascript">
